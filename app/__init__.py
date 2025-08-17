6e51ada28d1e9c1a59a65f0437efa7fe3d2ad954
Erol Mehmed <erol.mehmed07@gmail.com>
2025-08-17 16:16:18 +0300

    project initialisation

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from app.config.config import Config
+from .routes.media_routes import media_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Register blueprints
+    app.register_blueprint(media_bp, url_prefix="/media")
+
+    return app
