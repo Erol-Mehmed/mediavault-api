bfac223ed9628a5bb9fb69b5c2f8d1376aea034b
Erol Mehmed <erol.mehmed07@gmail.com>
2025-11-08 23:24:14 +0200

    switch to nestjs

--- app/controllers/media_controller.py
+++
@@ -1,9 +0,0 @@
-from app.services.media_service import MediaService
-
-
-class MediaController:
-    def __init__(self):
-        self.service = MediaService()
-
-    def get_all_media(self):
-        return self.service.fetch_all_media()
--- app/services/media_service.py
+++
@@ -1,9 +0,0 @@
-from app.repositories.media_repository import MediaRepository
-
-
-class MediaService:
-    def __init__(self):
-        self.repository = MediaRepository()
-
-    def fetch_all_media(self):
-        return self.repository.get_all()
